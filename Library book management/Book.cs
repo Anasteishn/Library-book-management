a749c1281f0b9367f1453bd1bcaaa144e8da9850
Анастасия Вещикова <veschikova.nastya@mail.ru>
2025-03-10 01:13:07 +0300

    add new class book

---
+++ Library book management/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+public class Book
+{
+    public string Author { get; set; }
+    public string Title { get; set; }
+    public string Year { get; set; }
+    public Book(string author, string title, string year)
+    {
+        Author = author;
+        Title = title;
+        Year = year;
+    }
+}
